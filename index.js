ee5050af20c26f0a9f2461867359b037a8ef588e
ianadriano1 <parussoloian2@gmail.com>
2023-04-26 16:24:31 -0300

    responsive completado

--- index.js
+++ index.js
@@ -1,11 +1,52 @@
-$(".sec3-slider").slick({
+/* $(".sec3-slider").slick({
   infinite: true,
   slidesToShow: 3,
   slidesToScroll: 1,
   autoplay: true,
   autoplaySpeed: 2000,
+}); */
+$(".sec3-slider").slick({
+  dots: true,
+  infinite: true,
+  speed: 300,
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  responsive: [
+    {
+      breakpoint: 1040,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: true,
+      },
+    },
+    {
+      breakpoint: 720,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1,
+      },
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1,
+      },
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+      },
+    },
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ],
 });
-
 const r = rolly({
   view: document.querySelector(".app"),
   native: true,
